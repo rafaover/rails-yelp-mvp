765e92fa0553e4ffd5ffc7868259d386df771ac8
Rafael Moreira <rafamsilva@gmail.com>
2023-03-25 12:45:54 +1100

    dev: validation on review and restaurant models

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: %w(0 1 2 3 4 5) }
+  validates :rating, presence: true, inclusion: { in: (0..5).to_a }
   validates :rating, numericality: { only_integer: true }
 end
