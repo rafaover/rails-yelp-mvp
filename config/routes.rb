bac9778494cd374f8dc1d12a63f6d9b7b3944a2a
Rafael Moreira <rafamsilva@gmail.com>
2023-03-25 13:02:34 +1100

    Routes updated with child review

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:index, :show, :create, :new] do
+    resources :reviews, only: [:new, :create]
+  end
 end
